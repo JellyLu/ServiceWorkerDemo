47aa011cc66c9a24a66b0223b837f5fa38b59769
JellyLu <634725546@qq.com>
2019-08-26 09:13:22 +0800

    add validation for email and confirm email

--- scripts/pages/form.js
+++ scripts/pages/form.js
@@ -1,8 +1,19 @@
-var form = document.getElementsByTagName('form');
-form.addEventListener('submit', function(event) {
+var inputs = document.getElementsByTagName("input");
+
+var form = document.getElementsByTagName("form")[0];
+form.addEventListener("submit", function(event) {
+  console.log("on submit");
   event.preventDefault();
-  if(!form.checkValidate()) {
+  const email = event.currentTarget[1].value;
+  const confirmEmail = event.currentTarget[2].value;
+
+  if(email !== confirmEmail) {
+    event.currentTarget[2].setCustomValidity("confirm email is not the same with email");
+  }
+
+  if(!form.checkValidity()) {
     alert("form is invalid");
     return false;
   }
+  alert("form is valid");
 });
