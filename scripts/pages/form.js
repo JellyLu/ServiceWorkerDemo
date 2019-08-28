c278139b937724ae1f565b3b96282dfe7127e126
JellyLu <634725546@qq.com>
2019-08-28 08:24:50 +0800

    display error when confirm email is not same with email

--- scripts/pages/form.js
+++ scripts/pages/form.js
@@ -1,4 +1,15 @@
 var inputs = document.getElementsByTagName("input");
+var errors = document.getElementsByClassName("error");
+
+inputs[1].addEventListener("input", function(event) {
+  inputs[2].setCustomValidity("");
+  errors[2].innerHTML = "";
+});
+
+inputs[2].addEventListener("input", function(event) {
+  inputs[2].setCustomValidity("");
+  errors[2].innerHTML = "";
+});
 
 var form = document.getElementsByTagName("form")[0];
 form.addEventListener("submit", function(event) {
@@ -6,14 +17,13 @@ form.addEventListener("submit", function(event) {
   event.preventDefault();
   const email = event.currentTarget[1].value;
   const confirmEmail = event.currentTarget[2].value;
-
   if(email !== confirmEmail) {
     event.currentTarget[2].setCustomValidity("confirm email is not the same with email");
+    errors[2].innerHTML = "confirm email is not the same with email";
   }
 
   if(!form.checkValidity()) {
     alert("form is invalid");
     return false;
   }
-  alert("form is valid");
 });
