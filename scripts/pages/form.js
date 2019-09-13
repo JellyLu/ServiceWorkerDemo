a335fd214d4a9376bdf4e3a1fa9f9fa4960d3513
JellyLu <yjlu@thoughtworks.com>
2019-09-13 08:30:56 +0800

    fix style for form

--- scripts/pages/form.js
+++ scripts/pages/form.js
@@ -54,12 +54,12 @@ var form = document.getElementsByTagName("form")[0];
 form.addEventListener("submit", function(event) {
   console.log("on submit");
   event.preventDefault();
-  // const email = event.currentTarget[1].value;
-  // const confirmEmail = event.currentTarget[2].value;
-  // if(email !== confirmEmail) {
-  //   event.currentTarget[2].setCustomValidity("confirm email is not the same with email");
-  //   errors[2].innerHTML = "confirm email is not the same with email";
-  // }
+  const email = event.currentTarget[1].value;
+  const confirmEmail = event.currentTarget[2].value;
+  if(email !== confirmEmail) {
+    event.currentTarget[2].setCustomValidity("confirm email is not the same with email");
+    errors[2].innerHTML = "confirm email is not the same with email";
+  }
 
   if(!form.checkValidity()) {
     alert("form is invalid");
